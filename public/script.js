570c39fc7258646e44bc5fdad7985b5415ac893b
ande7114 <ande7114@uni.sydney.edu.au>
2023-06-03 18:38:17 +1000

    Added an input form for users to journal entries

--- public/script.js
+++ public/script.js
@@ -1,121 +1,58 @@
-// // Setting up variables for our HTML elements using DOM selection
-// const form = document.getElementById("taskform");
-// const tasklist = document.getElementById("tasklist");
+// const daysTag = document.querySelector(".days"),
+//   currentDate = document.querySelector(".current-date"),
+//   prevNextIcon = document.querySelectorAll(".icons span");
 
-// form.addEventListener("submit", function(event) {
-//   event.preventDefault();
+// // getting new date, current year and month
+// let date = new Date(),
+//   currYear = date.getFullYear(),
+//   currMonth = date.getMonth();
 
-//   console.log(form.elements.taskType.value)
+// // storing full name of all months in array
+// const months = ["January", "February", "March", "April", "May", "June", "July",
+//   "August", "September", "October", "November", "December"];
 
-//   addTask(
-//     form.elements.taskName.value,
-//     form.elements.taskType.value,
-//     form.elements.taskRate.value,
-//     form.elements.taskTime.value,
-//     form.elements.taskClient.value,
-//   )
-//   console.log(taskList)
-// })
+// const renderCalendar = () => {
+//   let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+//     lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+//     lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+//     lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+//   let liTag = "";
 
-// function displayTask(task) {
-//   let item = document.createElement("li");
-//   item.setAttribute("data-id", task.id);
-//   item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
-
-//   tasklist.appendChild(item);
-
-//   // Clear the value of the input once the task has been added to the page
-//   form.reset();
-
-//   // Setup delete button DOM elements
-//   let delButton = document.createElement("button");
-//   let delButtonText = document.createTextNode("Delete");
-//   delButton.appendChild(delButtonText);
-//   item.appendChild(delButton); // Adds a delete button to every task
-
-//   // Listen for when the delete button is clicked
-//   delButton.addEventListener("click", function(event) {
-
-//     taskList.forEach(function(taskArrayElement, taskArrayIndex) {
-//       if (taskArrayElement.id == item.getAttribute('data-id')) {
-//         taskList.splice(taskArrayIndex, 1)
-//       }
-//     })
-
-//     // Make sure the deletion worked by logging out the whole array
-//     console.log(taskList)
-
-//     item.remove(); // Remove the task item from the page when button clicked
-//     // Because we used 'let' to define the item, this will always delete the right element
-
-//   })
-
-  
-// }
-
-
-// // Create an object called 'task'
-// // Populate the properties based on the provided data model
-
-// // Commented out now the object creation is included in the function
-
-// // var task = {
-// //   name: "Initial Sketches",
-// //   type: "Concept Ideation",
-// //   id: Date.now(),
-// //   date: new Date().toISOString(),
-// //   rate: 50,
-// //   time: 5,
-// //   client: "Google"
-// // }
-
-// // console.log(task);
-
-
-// // Create an array called 'taskList'
-// var taskList = [];
-
-// // Create a function called 'addTask'
-// // Give the function input parameters for: name, type, rate, time, client
-// // Paste your object definition from above in the function
-// // Replace the property values with the input paramaters
-// // Add the object to the taskList array
-
-// function addTask(name, type, rate, time, client) {
-
-//   // Creating the object with the usual property:value syntax
-//   // Create task object 
-//   // let task = {
-//   //   name: name,
-//   //   type: type,
-//   //   id: Date.now(),
-//   //   date: new Date().toISOString(),
-//   //   rate: rate,
-//   //   time: time,
-//   //   client: client
-//   // }
-
-//   // Creating the object, directly passing in the input parameters
-//   let task = {
-//     name,
-//     type,
-//     id: Date.now(),
-//     date: new Date().toISOString(),
-//     rate,
-//     time,
-//     client
+//   for (let i = firstDayofMonth; i > 0; i--) { // creating li of previous month last days
+//     liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
 //   }
 
-//   taskList.push(task);
-//   displayTask(task);
+//   for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+//     // adding active class to li if the current day, month, and year matched
+//     let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+//       && currYear === new Date().getFullYear() ? "active" : "";
+//     liTag += `<li class="${isToday}">${i}</li>`;
+//   }
 
+//   for (let i = lastDayofMonth; i < 6; i++) { // creating li of next month first days
+//     liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`
+//   }
+//   currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+//   daysTag.innerHTML = liTag;
 // }
+// renderCalendar();
 
-// // Call the function with test values for the input paramaters
-// addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+// prevNextIcon.forEach(icon => { // getting prev and next icons
+//   icon.addEventListener("click", () => { // adding click event on both icons
+//     // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+//     currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
 
-// // Log the array to the console.
-// console.log(taskList);
+//     if (currMonth < 0 || currMonth > 11) { // if current month is less than 0 or greater than 11
+//       // creating a new date of current year & month and pass it as date value
+//       date = new Date(currYear, currMonth, new Date().getDate());
+//       currYear = date.getFullYear(); // updating current year with new date year
+//       currMonth = date.getMonth(); // updating current month with new date month
+//     } else {
+//       date = new Date(); // pass the current date as date value
+//     }
+//     renderCalendar(); // calling renderCalendar function
+//   });
+// });
 
 const daysTag = document.querySelector(".days"),
   currentDate = document.querySelector(".current-date"),
@@ -172,3 +109,122 @@ prevNextIcon.forEach(icon => { // getting prev and next icons
     renderCalendar(); // calling renderCalendar function
   });
 });
+
+// Setting up variables for our HTML elements using DOM selection
+const form = document.getElementById("taskform");
+const tasklist = document.getElementById("tasklist");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  console.log(form.elements.taskType.value)
+
+  addTask(
+    form.elements.taskName.value,
+    form.elements.taskType.value,
+    form.elements.taskRate.value,
+    form.elements.taskTime.value,
+    form.elements.taskClient.value,
+  )
+  console.log(taskList)
+})
+
+function displayTask(task) {
+  let item = document.createElement("li");
+  item.setAttribute("data-id", task.id);
+  item.innerHTML = `<p><strong>${task.name}</strong><br>${task.type}</p>`;
+
+  tasklist.appendChild(item);
+
+  // Clear the value of the input once the task has been added to the page
+  form.reset();
+
+  // Setup delete button DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton); // Adds a delete button to every task
+
+  // Listen for when the delete button is clicked
+  delButton.addEventListener("click", function(event) {
+
+    taskList.forEach(function(taskArrayElement, taskArrayIndex) {
+      if (taskArrayElement.id == item.getAttribute('data-id')) {
+        taskList.splice(taskArrayIndex, 1)
+      }
+    })
+
+    // Make sure the deletion worked by logging out the whole array
+    console.log(taskList)
+
+    item.remove(); // Remove the task item from the page when button clicked
+    // Because we used 'let' to define the item, this will always delete the right element
+
+  })
+
+  
+}
+
+
+// // Create an object called 'task'
+// // Populate the properties based on the provided data model
+
+// // Commented out now the object creation is included in the function
+
+// // var task = {
+// //   name: "Initial Sketches",
+// //   type: "Concept Ideation",
+// //   id: Date.now(),
+// //   date: new Date().toISOString(),
+// //   rate: 50,
+// //   time: 5,
+// //   client: "Google"
+// // }
+
+// // console.log(task);
+
+
+// Create an array called 'taskList'
+var taskList = [];
+
+// Create a function called 'addTask'
+// Give the function input parameters for: name, type, rate, time, client
+// Paste your object definition from above in the function
+// Replace the property values with the input paramaters
+// Add the object to the taskList array
+
+function addTask(name, type, rate, time, client) {
+
+  // Creating the object with the usual property:value syntax
+  // Create task object 
+  // let task = {
+  //   name: name,
+  //   type: type,
+  //   id: Date.now(),
+  //   date: new Date().toISOString(),
+  //   rate: rate,
+  //   time: time,
+  //   client: client
+  // }
+
+  // Creating the object, directly passing in the input parameters
+  let task = {
+    name,
+    type,
+    id: Date.now(),
+    date: new Date().toISOString(),
+    rate,
+    time,
+    client
+  }
+
+  taskList.push(task);
+  displayTask(task);
+
+}
+
+// Call the function with test values for the input paramaters
+addTask("Initial Sketches", "Concept Ideation", 50, 5, "Google");
+
+// Log the array to the console.
+console.log(taskList);
